18543bcd5f821ee8acbfd2aa3b1ef116a7486c1a
Ali Başoğlu <alibasoglu1911@gmail.com>
2023-01-12 18:59:29 +0300

    Edit gradle files + add viewBinding utils

---
+++ app/src/main/java/com/alibasoglu/ciftlikpazarimandroid/utils/viewbinding/ViewBindingUtils.kt
@@ -0,0 +1,23 @@
+package com.alibasoglu.ciftlikpazarimandroid.utils.viewbinding
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+fun <T : ViewBinding> Fragment.viewBinding(viewBindingFactory: (View) -> T) =
+    FragmentViewBindingDelegate(this, viewBindingFactory)
+
+inline fun <T : ViewBinding> AppCompatActivity.viewBinding(
+    crossinline bindingInflater: (LayoutInflater) -> T
+) = lazy(LazyThreadSafetyMode.NONE) {
+    bindingInflater.invoke(layoutInflater)
+}
+
+inline fun <T : ViewBinding> ViewGroup.viewBinding(
+    crossinline inflater: (LayoutInflater, ViewGroup) -> T
+): T {
+    return inflater.invoke(LayoutInflater.from(context), this)
+}
