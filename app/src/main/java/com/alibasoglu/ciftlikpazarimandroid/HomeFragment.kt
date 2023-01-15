592d4f10174d7c810bf2390b3a36f812537de728
Ali Başoğlu <alibasoglu1911@gmail.com>
2023-01-15 16:32:02 +0300

    Fix nav pop stack after login -add NewAdvertFragment

--- app/src/main/java/com/alibasoglu/ciftlikpazarimandroid/HomeFragment.kt
+++ app/src/main/java/com/alibasoglu/ciftlikpazarimandroid/HomeFragment.kt
@@ -5,6 +5,8 @@ import android.view.View
 import androidx.core.view.isVisible
 import com.alibasoglu.ciftlikpazarimandroid.core.fragment.BaseFragment
 import com.alibasoglu.ciftlikpazarimandroid.core.fragment.FragmentConfiguration
+import com.alibasoglu.ciftlikpazarimandroid.databinding.FragmentHomeBinding
+import com.alibasoglu.ciftlikpazarimandroid.utils.viewbinding.viewBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,9 +15,41 @@ class HomeFragment : BaseFragment(R.layout.fragment_home) {
 
     override val fragmentConfiguration = FragmentConfiguration()
 
+    private val binding by viewBinding(FragmentHomeBinding::bind)
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initUi()
+    }
+
+    private fun initUi() {
         activity?.findViewById<BottomNavigationView>(R.id.bottomNavigationView)?.isVisible = true
+        with(binding) {
+            // TODO implement here
+            cowButton.setOnClickListener {
+
+            }
+            sheepButton.setOnClickListener {
+
+            }
+            chickenButton.setOnClickListener {
+
+            }
+            beeButton.setOnClickListener {
+
+            }
+            farmButton.setOnClickListener {
+
+            }
+            machineButton.setOnClickListener {
+
+            }
+        }
+
+    }
+
+    private fun navToCategoryAdvertsFragment(categorySelected: String) {
+
     }
 
 }
---
+++ app/src/main/java/com/alibasoglu/ciftlikpazarimandroid/adverts/ui/NewAdvertFragment.kt
@@ -0,0 +1,14 @@
+package com.alibasoglu.ciftlikpazarimandroid.adverts.ui
+
+import com.alibasoglu.ciftlikpazarimandroid.R
+import com.alibasoglu.ciftlikpazarimandroid.core.fragment.BaseFragment
+import com.alibasoglu.ciftlikpazarimandroid.core.fragment.FragmentConfiguration
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class NewAdvertFragment : BaseFragment(R.layout.fragment_new_advert) {
+
+    override val fragmentConfiguration = FragmentConfiguration()
+
+
+}
