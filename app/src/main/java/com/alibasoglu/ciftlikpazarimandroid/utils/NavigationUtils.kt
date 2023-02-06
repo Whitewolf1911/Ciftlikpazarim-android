496fa1541c1aed0993986d71510da45e6878a4bd
Ali Başoğlu <alibasoglu1911@gmail.com>
2023-02-06 15:48:05 +0300

    💄|Add navigation animations

--- app/src/main/java/com/alibasoglu/ciftlikpazarimandroid/utils/NavigationUtils.kt
+++ app/src/main/java/com/alibasoglu/ciftlikpazarimandroid/utils/NavigationUtils.kt
@@ -1,13 +1,21 @@
 package com.alibasoglu.ciftlikpazarimandroid.utils
 
-import androidx.annotation.NonNull
 import androidx.navigation.NavController
 import androidx.navigation.NavDirections
+import androidx.navigation.NavOptions
+import com.alibasoglu.ciftlikpazarimandroid.R
 
-fun NavController.navigateSafe(@NonNull directions: NavDirections, onError: (() -> Unit)? = null) {
+fun NavController.navigateSafe(directions: NavDirections, onError: (() -> Unit)? = null) {
     try {
-        navigate(directions)
+        navigate(directions, navOptions)
     } catch (exception: IllegalArgumentException) {
         onError?.invoke()
     }
 }
+
+private val navOptions = NavOptions.Builder().apply {
+    setEnterAnim(R.anim.slide_in)
+    setExitAnim(R.anim.fade_out)
+    setPopEnterAnim(R.anim.fade_in)
+    setPopExitAnim(R.anim.slide_out)
+}.build()
