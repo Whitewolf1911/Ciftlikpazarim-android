db6c8b3ad8af8f5509aac039ef3286d5edb04681
Ali Başoğlu <alibasoglu1911@gmail.com>
2023-02-08 17:17:23 +0300

    🚧|Add MessagePreviewItem with compose

---
+++ app/src/main/java/com/alibasoglu/ciftlikpazarimandroid/messaging/ui/MessagePreviewItem.kt
@@ -0,0 +1,39 @@
+package com.alibasoglu.ciftlikpazarimandroid.messaging.ui
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.alibasoglu.ciftlikpazarimandroid.messaging.model.MessagePreview
+
+@Composable
+fun MessagePreviewItem(
+    messagePreview: MessagePreview,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .border(border = BorderStroke(1.dp, Color.Green), shape = RoundedCornerShape(50.dp))
+            .fillMaxWidth()
+            .padding(16.dp)
+            .clipToBounds()
+    ) {
+        Text(messagePreview.username, fontWeight = FontWeight.Bold, fontSize = 18.sp)
+        Spacer(modifier = Modifier.height(2.dp))
+        Text(messagePreview.lastMessage, maxLines = 1, overflow = TextOverflow.Ellipsis)
+    }
+
+}
--- app/src/main/java/com/alibasoglu/ciftlikpazarimandroid/messaging/ui/MessagesPreviewFragment.kt
+++ app/src/main/java/com/alibasoglu/ciftlikpazarimandroid/messaging/ui/MessagesPreviewFragment.kt
@@ -2,11 +2,21 @@ package com.alibasoglu.ciftlikpazarimandroid.messaging.ui
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.viewModels
 import com.alibasoglu.ciftlikpazarimandroid.R
 import com.alibasoglu.ciftlikpazarimandroid.core.fragment.BaseFragment
 import com.alibasoglu.ciftlikpazarimandroid.core.fragment.FragmentConfiguration
 import com.alibasoglu.ciftlikpazarimandroid.core.fragment.ToolbarConfiguration
+import com.alibasoglu.ciftlikpazarimandroid.databinding.FragmentMessagesPreviewBinding
+import com.alibasoglu.ciftlikpazarimandroid.messaging.model.MessagePreview
+import com.alibasoglu.ciftlikpazarimandroid.utils.viewbinding.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,6 +25,8 @@ class MessagesPreviewFragment : BaseFragment(R.layout.fragment_messages_preview)
     private val toolbarConfiguration = ToolbarConfiguration(titleResId = R.string.messages)
 
     override val fragmentConfiguration = FragmentConfiguration(toolbarConfiguration)
+
+    private val binding by viewBinding(FragmentMessagesPreviewBinding::bind)
 
     private val messagesPreviewViewModel by viewModels<MessagesPreviewViewModel>()
 
@@ -25,9 +37,29 @@ class MessagesPreviewFragment : BaseFragment(R.layout.fragment_messages_preview)
         messagesPreviewViewModel.getMessagesPreview()
     }
 
-
     private fun initUi() {
-        // TODO initUi
+        binding.composeView.apply {
+            setContent {
+                val dummyData =
+                    MessagePreview(
+                        id = "id",
+                        username = "Ahmet Mehmet",
+                        lastMessage = "Selamlar ustam bu traktor en son kaca olur? hadi bi cevap ustam"
+                    )
+                LazyColumn(contentPadding = PaddingValues(bottom = 80.dp) ) {
+                    items(count = 10) {
+                        MessagePreviewItem(
+                            messagePreview = dummyData,
+                            modifier = Modifier
+                                .padding(12.dp)
+                                .clickable {
+                                    navToMessageDetailsFragment()
+                                },
+                        )
+                    }
+                }
+            }
+        }
     }
 
     private fun initObservers() {
@@ -35,7 +67,7 @@ class MessagesPreviewFragment : BaseFragment(R.layout.fragment_messages_preview)
     }
 
     private fun navToMessageDetailsFragment() {
-        // TODO nav to conversation
+        Toast.makeText(context, "you clicked me", Toast.LENGTH_SHORT).show()
     }
 
 }
