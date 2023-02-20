7c3c7cb584626418c5afcf20759b4440a1e3dcf0
Ali Başoğlu <alibasoglu1911@gmail.com>
2023-02-20 19:31:34 +0300

    Implement ChangePasswordFragment UI

--- app/src/main/java/com/alibasoglu/ciftlikpazarimandroid/user/ui/updateinfo/ChangePasswordFragment.kt
+++ app/src/main/java/com/alibasoglu/ciftlikpazarimandroid/user/ui/updateinfo/ChangePasswordFragment.kt
@@ -1,9 +1,28 @@
 package com.alibasoglu.ciftlikpazarimandroid.user.ui.updateinfo
 
+import android.os.Bundle
+import android.view.View
 import com.alibasoglu.ciftlikpazarimandroid.R
 import com.alibasoglu.ciftlikpazarimandroid.core.fragment.BaseFragment
 import com.alibasoglu.ciftlikpazarimandroid.core.fragment.FragmentConfiguration
+import com.alibasoglu.ciftlikpazarimandroid.databinding.FragmentChangePasswordBinding
+import com.alibasoglu.ciftlikpazarimandroid.utils.viewbinding.viewBinding
 
 class ChangePasswordFragment : BaseFragment(R.layout.fragment_change_password) {
     override val fragmentConfiguration = FragmentConfiguration()
+
+    private val binding by viewBinding(FragmentChangePasswordBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUI()
+    }
+
+    private fun initUI(){
+        with(binding){
+            submitButton.setOnClickListener {
+                //TODO show dialog to approve
+            }
+        }
+    }
 }
